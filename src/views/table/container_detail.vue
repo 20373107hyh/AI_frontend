33bd2849541354e183fbce9159e2d0dbe31ee27d
huyiheng <3091296503@qq.com>
2024-04-21 20:41:21 +0800

    镜像模块改进

--- src/views/table/container_detail.vue
+++ src/views/table/container_detail.vue
@@ -16,12 +16,44 @@
       </el-input>
       <button type="submit">Submit</button>
     </form>
-    <ul v-if="uploaded_files.length > 0">
+    <!-- <ul v-if="uploaded_files.length > 0">
       <li v-for="(file, index) in uploaded_files" :key="index">
         {{ file.filename }} &nbsp;&nbsp; &nbsp; &nbsp; &nbsp;  {{ file.path }}  
         <button @click="onDelete(file.path)">Delete</button>
       </li>
-    </ul>
+    </ul> -->
+    <el-table 
+      :data="uploaded_files" 
+      v-loading="listLoading"
+      @selection-change="handleSelectionChange">
+      <el-table-column
+        type="selection"
+        :selectable="isSelectable"
+        width="55">
+      </el-table-column>
+      <el-table-column 
+        prop="name" 
+        label="文件名">
+        <template slot-scope="scope">
+          {{ scope.row.filename}}
+        </template>
+      </el-table-column>
+      <el-table-column 
+        prop="path" 
+        label="文件路径">
+        <template slot-scope="scope">
+          {{ scope.row.path}}
+        </template>
+      </el-table-column>
+      <el-table-column 
+        prop="operation" 
+        label="操作">
+        <template slot-scope="scope">
+          <el-button @click="onDelete([scope.row.path])">Delete</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-button @click="onDeleteSelectedFiles">删除所选</el-button>
     <el-button type="primary" @click="handleEnter()"> 进入 </el-button>
     <el-button type="primary" @click="handleReturn()"> 返回 </el-button>
   </div>
@@ -32,13 +64,19 @@ export default {
   data() {
     return {
       files: [], //正在上传的文件
+      selectedFiles: [],
       uploaded_files:[], //已上传的文件
       container_info: {},
       path: '',
+      listLoading: true,
     };
   },
   methods: {
+    handleSelectionChange(selected) {
+      this.selectedFiles = selected.map(file => file.path);
+    },
     loadFiles(name){
+      this.listLoading = true
       console.log(name)
       const formData = new FormData()
       let user_id = localStorage.getItem('user_id')
@@ -51,12 +89,18 @@ export default {
       }).then(res => {
         console.log(res)
         this.uploaded_files = res.data.data
+        this.listLoading = false
       })
       this.files = []
     },
     onFileChange(e) {
       this.files = [...e.target.files];
       console.log(this.files)
+    },
+    isSelectable(file) {
+      // 这个函数决定哪些文件可以被选中
+      // 不写代表允许所有文件被选中
+      return true;
     },
     onSubmit() {
       const formData = new FormData();
@@ -68,6 +112,7 @@ export default {
       for (let i = 0; i < this.files.length; i++) {
         let file = this.files[i];
         formData.append('file[]', file);
+        formData.append('path[]', file.webkitRelativePath || file.name); // 添加文件的相对路径
       }
       this.$axios({
           method: 'post',
@@ -85,12 +130,13 @@ export default {
 
     },
     onDelete(path){
+      console.log(path)
       const formData = new FormData();
       let user_id = localStorage.getItem('user_id')
       let container_name = this.container_info.container_name
       formData.append('user_id', user_id)
       formData.append('container_name', container_name)
-      formData.append('file_path', path)
+      formData.append('file_paths', JSON.stringify(path))
       this.$axios({
           method: 'post',
           url: '/teacher/delete_file/',
@@ -100,6 +146,10 @@ export default {
           this.loadFiles(container_name)
         })
           .catch(error => console.log(error));
+    },
+    onDeleteSelectedFiles(){
+      console.log(this.selectedFiles)
+      this.onDelete(this.selectedFiles); // Call onDelete with the paths of the selected files
     },
     search_container(){
       const formData = new FormData();
